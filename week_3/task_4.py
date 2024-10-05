b28309e6f24398b1631f4b7417651c3432bc4fa4
Olesia <o.koshurinova@gmail.com>
2024-10-05 19:59:58 +0300

    week_3 tasks

---
+++ week_3/task_4.py
@@ -0,0 +1,21 @@
+import heapq
+
+
+class Solution(object):
+    def kSmallestPairs(self, nums1, nums2, k):
+        res = []
+        pq = []
+
+        for num in nums1:
+            heapq.heappush(pq, (num + nums2[0], 0))
+
+        while k > 0 and pq:
+            sum_, j = heapq.heappop(pq)
+            res.append([sum_ - nums2[j], nums2[j]])
+
+            if j + 1 < len(nums2):
+                heapq.heappush(pq, (sum_ - nums2[j] + nums2[j + 1], j + 1))
+
+            k -= 1
+
+        return res
